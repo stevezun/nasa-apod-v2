fd8b32ec7d98ddb63deaf8dde9208f0a27cc803a
Nick Bennett <nick@nickbenn.com>
2020-01-30 13:59:54 -0700

    Implemented date picking.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/ImageFragment.java
@@ -14,12 +14,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import edu.cnm.deepdive.android.DateTimePickerFragment;
+import edu.cnm.deepdive.android.DateTimePickerFragment.Mode;
+import edu.cnm.deepdive.android.DateTimePickerFragment.OnChangeListener;
 import edu.cnm.deepdive.nasaapod.R;
 import edu.cnm.deepdive.nasaapod.model.Apod;
 import edu.cnm.deepdive.nasaapod.viewmodel.MainViewModel;
+import java.util.Calendar;
 
 public class ImageFragment extends Fragment {
-  
+
   private WebView contentView;
   private MainViewModel viewModel;
   private ProgressBar loading;
@@ -32,6 +36,7 @@ public class ImageFragment extends Fragment {
     loading = root.findViewById(R.id.loading);
     calendar = root.findViewById(R.id.calendar);
     setupWebView(root);
+    setupCalendarPicker(Calendar.getInstance());
     return root;
   }
 
@@ -40,7 +45,12 @@ public class ImageFragment extends Fragment {
     super.onViewCreated(view, savedInstanceState);
     viewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
     viewModel.getApod().observe(getViewLifecycleOwner(),
-        (Apod apod) -> contentView.loadUrl(apod.getUrl()));
+        (apod) -> {
+          contentView.loadUrl(apod.getUrl());
+          Calendar calendar = Calendar.getInstance();
+          calendar.setTime(apod.getDate());
+          setupCalendarPicker(calendar);
+        });
   }
 
   private void setupWebView(View root) {
@@ -63,6 +73,19 @@ public class ImageFragment extends Fragment {
     settings.setDisplayZoomControls(false);
     settings.setUseWideViewPort(true);
     settings.setLoadWithOverviewMode(true);
+  }
+
+  private void setupCalendarPicker(Calendar calendar) {
+    this.calendar.setOnClickListener((v) -> {
+      DateTimePickerFragment fragment = new DateTimePickerFragment();
+      fragment.setCalendar(calendar);
+      fragment.setMode(Mode.DATE);
+      fragment.setOnChangeListener((cal) -> {
+        loading.setVisibility(View.VISIBLE);
+        viewModel.setApodDate(cal.getTime());
+      });
+      fragment.show(getChildFragmentManager(), fragment.getClass().getName());
+    });
   }
 
 }
