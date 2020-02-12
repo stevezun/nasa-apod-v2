1e5efc9e5ced68c3aae540b0ac429f530c9ce851
Steven Zuniga <szuniga@cnm.edu>
2020-02-12 10:25:55 -0700

    Refactor loading to activity. (in progress)

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/ImageFragment.java
@@ -31,8 +31,6 @@ public class ImageFragment extends Fragment {
 
   private WebView contentView;
   private MainViewModel viewModel;
-  private ProgressBar loading;
-  private FloatingActionButton calendar;
   private Apod apod;
 
   @Override
@@ -46,10 +44,7 @@ public class ImageFragment extends Fragment {
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
     View root = inflater.inflate(R.layout.fragment_image, container, false);
-    loading = root.findViewById(R.id.loading);
-    calendar = root.findViewById(R.id.calendar);
     setupWebView(root);
-    setupCalendarPicker(Calendar.getInstance());
     return root;
   }
 
@@ -60,13 +55,10 @@ public class ImageFragment extends Fragment {
     viewModel.getApod().observe(getViewLifecycleOwner(),
         (apod) -> {
           contentView.loadUrl(apod.getUrl());
-          Calendar calendar = Calendar.getInstance();
-          calendar.setTime(apod.getDate());
-          setupCalendarPicker(calendar);
           this.apod = apod;
         });
     viewModel.getThrowable().observe(getViewLifecycleOwner(), (throwable) -> {
-      loading.setVisibility(View.GONE);
+      ((MainActivity) getActivity()).setProgressVisibility(View.GONE);
       Toast toast = Toast.makeText(getActivity(),
           getString(R.string.error_message, throwable.getMessage()), Toast.LENGTH_LONG);
       toast.setGravity(Gravity.BOTTOM, 0,
@@ -114,7 +106,7 @@ public class ImageFragment extends Fragment {
 
       @Override
       public void onPageFinished(WebView view, String url) {
-        loading.setVisibility(View.GONE);
+        ((MainActivity) getActivity()).setProgressVisibility(View.GONE);
         Toast toast = Toast.makeText(getActivity(), apod.getTitle(), Toast.LENGTH_LONG);
         toast.setGravity(Gravity.BOTTOM, 0, (int) getContext().getResources()
             .getDimensionPixelOffset(R.dimen.toast_vertical_margin));
@@ -128,19 +120,6 @@ public class ImageFragment extends Fragment {
     settings.setDisplayZoomControls(false);
     settings.setUseWideViewPort(true);
     settings.setLoadWithOverviewMode(true);
-  }
-
-  private void setupCalendarPicker(Calendar calendar) {
-    this.calendar.setOnClickListener((v) -> {
-      DateTimePickerFragment fragment = new DateTimePickerFragment();
-      fragment.setCalendar(calendar);
-      fragment.setMode(Mode.DATE);
-      fragment.setOnChangeListener((cal) -> {
-        loading.setVisibility(View.VISIBLE);
-        viewModel.setApodDate(cal.getTime());
-      });
-      fragment.show(getChildFragmentManager(), fragment.getClass().getName());
-    });
   }
 
 }
--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/MainActivity.java
@@ -1,29 +1,79 @@
 package edu.cnm.deepdive.nasaapod.controller;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import androidx.lifecycle.ViewModelProvider;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import edu.cnm.deepdive.android.DateTimePickerFragment;
+import edu.cnm.deepdive.android.DateTimePickerFragment.Mode;
 import edu.cnm.deepdive.nasaapod.R;
+import edu.cnm.deepdive.nasaapod.viewmodel.MainViewModel;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+  private MainViewModel viewModel;
+  private BottomNavigationView navView;
+  private NavController navController;
+  private ProgressBar loading;
+  private FloatingActionButton calendarFab;
+  private Calendar calendar;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    BottomNavigationView navView = findViewById(R.id.nav_view);
+    setupNavigation();
+    setupViewModel();
+    loading = findViewById(R.id.loading);
+    setupCalendarPicker();
+  }
+
+  private void setupViewModel() {
+    viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+    viewModel.getApod().observe(this, (apod) -> calendar.setTime(apod.getDate()));
+  }
+
+  private void setupNavigation() {
+    navView = findViewById(R.id.nav_view);
     // Passing each menu ID as a set of Ids because each
     // menu should be considered as top level destinations.
     AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
         R.id.navigation_image, R.id.navigation_history)
         .build();
-    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+    navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     NavigationUI.setupWithNavController(navView, navController);
+  }
+
+  public void loadApod(Date date) {
+    setProgressVisibility(View.VISIBLE);
+    viewModel.setApodDate(date);
+    navController.navigate(R.id.navigation_image);
+  }
+
+  public void setProgressVisibility(int visibility) {
+    loading.setVisibility(visibility);
+  }
+
+  private void setupCalendarPicker() {
+    calendar = Calendar.getInstance();
+    calendarFab = findViewById(R.id.calendar_fab);
+    calendarFab.setOnClickListener((v) -> {
+      DateTimePickerFragment fragment = new DateTimePickerFragment();
+      fragment.setCalendar(calendar);
+      fragment.setMode(Mode.DATE);
+      fragment.setOnChangeListener((cal) -> loadApod(cal.getTime()));
+      fragment.show(getSupportFragmentManager(), fragment.getClass().getName());
+    });
   }
 
 }
